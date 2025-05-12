f441586d2865e656eef074db8b22da147ec36c2f
Rumi1013 <bgconscious@gmail.com>
2025-05-11 23:36:42 -0400

    chore: configure project with tai   lwind and next.js

--- vizify/next.config.js
+++ vizify/next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ['v0.blob.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  // No custom output directory - let Next.js use the default
+};
 
-module.exports = nextConfig
+export default nextConfig;
